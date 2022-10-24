dcf52dd2399fa91dfed3239b0db84f653a2b4a3b
danyawn <thephantomwarrio02@gmail.com>
2022-10-25 02:33:11 +0700

    change the calculation formula

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 function bookPuchase(price, amount, boolTax, title) {
   let tDisc = price / amount;
-  let afterDisc = price - tDisc;
+  let totalPrice = price * amount;
+  let afterDisc = totalPrice - tDisc;
   let tax = afterDisc / 10;
+  let total = afterDisc + tax;
   if (boolTax == true) {
     let total = afterDisc + tax;
-    console.log("Title : ", title);
-    console.log("Price : ", total);
+    console.log("Title    : ", title);
+    console.log("Price    : ", price);
+    console.log("Purchase : ", amount);
+    console.log("Price    : ", total);
   } else {
-    console.log("Title : ", title);
-    console.log("Price : ", afterDisc);
+    console.log("Title    : ", title);
+    console.log("Price    : ", price);
+    console.log("Purchase : ", amount);
+    console.log("Price    : ", afterDisc);
   }
 }
 
