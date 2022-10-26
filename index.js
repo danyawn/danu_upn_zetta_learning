22c11b4c17ca77b8d7d272844f7ccde981d26f3c
danyawn <thephantomwarrio02@gmail.com>
2022-10-26 14:58:36 +0700

    submit js day 3

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
 var stock = 20;
 
-function bookPuchase(price, amount, title) {
+// Function Term of Credit
+function Credit(total) {
+  console.log("Terms of Credit\n---------------");
+  for (let count = 2; count <= 12; count++) {
+    if (count % 2 == 0) {
+      let credit = total / count;
+      console.log("Credit in ", count, " month = Rp.", credit, "/Month");
+    }
+  }
+  console.log("---------------------------------------\n\n");
+}
+
+// Function Purcahse Book
+function bookPurchase(price, amount, title) {
   if (stock >= amount) {
     let tDisc = price / amount;
     let totalPrice = price * amount;
     let afterDisc = totalPrice - tDisc;
     let tax = afterDisc / 10;
     let total = afterDisc + tax;
+    var Pay = total;
     console.log("Title    : ", title);
     console.log("Order    : ", amount, " pcs");
-    console.log("Price    : ", price);
-    console.log("Discount : ", tDisc);
-    console.log("Tax      : ", tax);
-    console.log("Total Price : ", total);
-
+    console.log("Price    : Rp.", price);
+    console.log("Discount : Rp.", tDisc);
+    console.log("Tax      : Rp.", tax);
+    console.log("Total Price : Rp.", total);
+    console.log("------------------------------\n");
     for (let index = stock; index > amount; index--) {
       stock--;
       if (stock == 0) {
@@ -25,11 +39,9 @@ function bookPuchase(price, amount, title) {
   } else if (stock == 0) {
     console.log("You can't purchase this book because this book are sold out.");
   }
+  return Pay;
 }
 
 console.log("2 times purchased\n");
-bookPuchase(10000, 10, "Home Alone");
-console.log("\n");
-bookPuchase(10000, 5, "Home Alone");
-console.log("\n");
-bookPuchase(10000, 10, "Home Alone");
+Credit(bookPurchase(10000, 16, "Home Alone"));
+Credit(bookPurchase(20000, 10, "Sooner or Later"));
