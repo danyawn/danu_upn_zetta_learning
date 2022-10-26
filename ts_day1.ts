e508f386e87d0fae5d9b95789943ed46aaa7e252
danyawn <thephantomwarrio02@gmail.com>
2022-10-27 02:01:19 +0700

    submit typescript_day1

---
+++ ts_day1.ts
@@ -0,0 +1,10 @@
+function Combination(learn: string, num: number, nums: number) {
+  console.log(learn);
+  console.log(num);
+  console.log(nums);
+
+  console.log("\nThe Return is : ");
+  return "Typescript";
+}
+
+console.log(Combination("Learning Typescript is different than Javascript", 9, 19));
