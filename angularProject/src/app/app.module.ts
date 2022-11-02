02ff66512012073ad691c5bac0e7a11976cee06a
danyawn <thephantomwarrio02@gmail.com>
2022-11-02 08:20:21 +0700

    Default Angular Project

--- angularProject/src/app/app.module.ts
+++ angularProject/src/app/app.module.ts
@@ -3,20 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { Module1Component } from './module1/module1.component';
-import { Module2Component } from './module2/module2.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    Module1Component,
-    Module2Component
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
